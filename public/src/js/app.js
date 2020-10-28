7ab7a26ceb035b5e25a85810f2c6305bc4de62a1
Nikki <nikki.satopdx@gmail.com>
2020-10-27 17:41:49 -0700

    subscription firebase vapid

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,71 +1,119 @@
 var deferredPrompt;
 var enableNotificationsButtons = document.querySelectorAll(
-	'.enable-notifications'
+  '.enable-notifications'
 );
 
 if (!window.Promise) {
-	window.Promise = Promise;
+  window.Promise = Promise;
 }
 
 //Checking if service worker is available
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('/sw.js')
-		.then(() => {
-			console.log('Service worker registered!');
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('Service worker registered!');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 window.addEventListener('beforeinstallprompt', (e) => {
-	console.log('beforeinstallprompt fired');
-	e.preventDefault();
-	deferredPrompt = e;
-	return false;
+  console.log('beforeinstallprompt fired');
+  e.preventDefault();
+  deferredPrompt = e;
+  return false;
 });
 
 function displayConfirmNotification() {
-	if ('serviceWorker' in navigator) {
-		var options = {
-			body: 'You successfully subscribed to our Notification service!',
-			icon: '/src/images/icons/app-icon-96x96.png',
-			image: '/src/images/sf-boat.jpg',
-			dir: 'ltr',
-			lang: 'en-US',
-			vibrate: [100, 50, 200],
-			badge: '/src/images/icons/app-icon-96x96.png',
-			tag: 'confirm-notification',
-			renotify: true,
-			actions: [
-				{ action: 'confirm', title: 'Okay', icon: '/src/images/sf-boat.jpg' },
-				{ action: 'cancel', title: 'Cancel', icon: '/src/images/sf-boat.jpg' },
-			],
-		};
-		navigator.serviceWorker.ready.then((swreg) => {
-			swreg.showNotification('Successfully subscribed (From SW)', options);
-		});
-	}
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to our Notification service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US',
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        { action: 'confirm', title: 'Okay', icon: '/src/images/sf-boat.jpg' },
+        { action: 'cancel', title: 'Cancel', icon: '/src/images/sf-boat.jpg' },
+      ],
+    };
+    navigator.serviceWorker.ready.then((swreg) => {
+      swreg.showNotification('Successfully subscribed', options);
+    });
+  }
+}
+
+function configPushSub() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+  var reg;
+  navigator.serviceWorker.ready
+    .then((swreg) => {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then((sub) => {
+      if (sub === null) {
+        //Create a new subscription
+        var vapidPublicKey =
+          'BExo2PfaTCWLx2z2hiddkM4f1PgD3sBroxaPaDdRDTQ2SL6JaxxHTzri0V40fTdZUdLjaaHbFwpRiwk5Lry-dY8';
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey,
+        });
+      } else {
+        //we have a subscription
+      }
+    })
+    .then((newSub) => {
+      return fetch(
+        'https://pwa-udemy-68dcb.firebaseio.com/subscriptions.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify(newSub),
+        }
+      );
+    })
+    .then((res) => {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function askForNotificationPermission() {
-	Notification.requestPermission((result) => {
-		console.log('User Choice', result);
-		if (result !== 'granted') {
-			console.log('No Notification permission granted!');
-		} else {
-			displayConfirmNotification();
-		}
-	});
+  Notification.requestPermission((result) => {
+    console.log('User Choice', result);
+    if (result !== 'granted') {
+      console.log('No Notification permission granted!');
+    } else {
+      configPushSub();
+      //displayConfirmNotification();
+    }
+  });
 }
 
 if ('Notification' in window) {
-	for (var i = 0; i < enableNotificationsButtons.length; i++) {
-		enableNotificationsButtons[i].style.display = 'inline-block';
-		enableNotificationsButtons[i].addEventListener(
-			'click',
-			askForNotificationPermission
-		);
-	}
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = 'inline-block';
+    enableNotificationsButtons[i].addEventListener(
+      'click',
+      askForNotificationPermission
+    );
+  }
 }
--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,24 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v31';
+var CACHE_STATIC_NAME = 'static-v32';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
-	'/',
-	'/index.html',
-	'/offline.html',
-	'/src/js/app.js',
-	'/src/js/feed.js',
-	'/src/js/idb.js',
-	'/src/js/promise.js',
-	'/src/js/fetch.js',
-	'/src/js/material.min.js',
-	'/src/css/app.css',
-	'/src/css/feed.css',
-	'/src/images/main-image.jpg',
-	'https://fonts.googleapis.com/css?family=Roboto:400,700',
-	'https://fonts.googleapis.com/icon?family=Material+Icons',
-	'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/idb.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
 ];
 
 //function trimCache(cacheName, maxItems) {
@@ -33,32 +33,32 @@ var STATIC_FILES = [
 
 //Service worker Installing
 self.addEventListener('install', (e) => {
-	console.log('[Service Worker] Installing Service Worker....', e);
-	e.waitUntil(
-		caches.open(CACHE_STATIC_NAME).then((cache) => {
-			console.log('[SERVICE WORKER] PRECACHING APP SHELL');
-			cache.addAll([STATIC_FILES]);
-		})
-	);
+  console.log('[Service Worker] Installing Service Worker....', e);
+  e.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then((cache) => {
+      console.log('[SERVICE WORKER] PRECACHING APP SHELL');
+      cache.addAll([STATIC_FILES]);
+    })
+  );
 });
 
 //Service worker activating
 self.addEventListener('activate', (e) => {
-	console.log('[Service Worker] Activating Service Worker....', e);
+  console.log('[Service Worker] Activating Service Worker....', e);
 
-	e.waitUntil(
-		caches.keys().then((keyList) => {
-			return Promise.all(
-				keyList.map((key) => {
-					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-						console.log('[SERVICE WORKER] removing old cache.', key);
-						return caches.delete(key);
-					}
-				})
-			);
-		})
-	);
-	return self.clients.claim();
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[SERVICE WORKER] removing old cache.', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 //function isInArray(string, array) {
@@ -70,124 +70,124 @@ self.addEventListener('activate', (e) => {
 //}
 
 function isInArray(string, array) {
-	var cachePath;
-	if (string.indexOf(self.origin) === 0) {
-		// request targets domain where we serve the page from (i.e. NOT a CDN)
-		console.log('matched ', string);
-		cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
-	} else {
-		cachePath = string; // store the full request (for CDNs)
-	}
-	return array.indexOf(cachePath) > -1;
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
+  }
+  return array.indexOf(cachePath) > -1;
 }
 
 //Cache then network & dynamic caching
 self.addEventListener('fetch', (event) => {
-	var url = 'https://pwa-udemy-68dcb.firebaseio.com/posts';
+  var url = 'https://pwa-udemy-68dcb.firebaseio.com/posts';
 
-	if (event.request.url.indexOf(url) > -1) {
-		event.respondWith(
-			fetch(event.request).then((res) => {
-				var clonedRes = res.clone();
-				clearAllData('posts')
-					.then(() => {
-						return clonedRes.json();
-					})
-					.then((data) => {
-						for (var key in data) {
-							writeData('posts', data[key]);
-						}
-					});
-				return res;
-			})
-		);
-	} else if (isInArray(event.request.url, STATIC_FILES)) {
-		event.respondWith(
-			fetch(event.request).catch(() => {
-				return caches.match(event.request);
-			})
-		);
-	} else {
-		event.respondWith(
-			caches.match(event.request).then((response) => {
-				if (response) {
-					return response;
-				} else {
-					return fetch(event.request)
-						.then((res) => {
-							return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
-								cache.put(event.request.url, res.clone());
-								return res;
-							});
-						})
-						.catch((err) => {
-							return caches.open(CACHE_STATIC_NAME).then((cache) => {
-								if (event.request.headers.get('accept').includes('text/html')) {
-									return cache.match('/offline.html');
-								}
-							});
-						});
-				}
-			})
-		);
-	}
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      fetch(event.request).then((res) => {
+        var clonedRes = res.clone();
+        clearAllData('posts')
+          .then(() => {
+            return clonedRes.json();
+          })
+          .then((data) => {
+            for (var key in data) {
+              writeData('posts', data[key]);
+            }
+          });
+        return res;
+      })
+    );
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(event.request);
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then((res) => {
+              return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+                cache.put(event.request.url, res.clone());
+                return res;
+              });
+            })
+            .catch((err) => {
+              return caches.open(CACHE_STATIC_NAME).then((cache) => {
+                if (event.request.headers.get('accept').includes('text/html')) {
+                  return cache.match('/offline.html');
+                }
+              });
+            });
+        }
+      })
+    );
+  }
 });
 
 self.addEventListener('sync', (e) => {
-	console.log('[Service Worker] Background syncing', e);
-	if (e.tag === 'sync-new-posts') {
-		console.log('[Service Worker] Syncing new Posts');
-		e.waitUntil(
-			readAllData('sync-posts').then((data) => {
-				for (var dt of data) {
-					fetch(
-						'https://us-central1-pwa-udemy-68dcb.cloudfunctions.net/storePostData',
-						{
-							method: 'POST',
-							headers: {
-								'Content-Type': 'application/json',
-								Accept: 'application/json',
-							},
-							body: JSON.stringify({
-								id: dt.id,
-								title: dt.title,
-								location: dt.location,
-								image:
-									'https://firebasestorage.googleapis.com/v0/b/pwa-udemy-68dcb.appspot.com/o/sf-boat.jpg?alt=media&token=932e3373-f395-4ad4-968d-bc874662f8c0',
-							}),
-						}
-					)
-						.then((res) => {
-							console.log('Sent data', res);
-							if (res.ok) {
-								res.json().then((resData) => {
-									deleteItemFromData('sync-posts', resData.id);
-								});
-							}
-						})
-						.catch((err) => {
-							console.log('Error while sending data', err);
-						});
-				}
-			})
-		);
-	}
+  console.log('[Service Worker] Background syncing', e);
+  if (e.tag === 'sync-new-posts') {
+    console.log('[Service Worker] Syncing new Posts');
+    e.waitUntil(
+      readAllData('sync-posts').then((data) => {
+        for (var dt of data) {
+          fetch(
+            'https://us-central1-pwa-udemy-68dcb.cloudfunctions.net/storePostData',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+              },
+              body: JSON.stringify({
+                id: dt.id,
+                title: dt.title,
+                location: dt.location,
+                image:
+                  'https://firebasestorage.googleapis.com/v0/b/pwa-udemy-68dcb.appspot.com/o/sf-boat.jpg?alt=media&token=932e3373-f395-4ad4-968d-bc874662f8c0',
+              }),
+            }
+          )
+            .then((res) => {
+              console.log('Sent data', res);
+              if (res.ok) {
+                res.json().then((resData) => {
+                  deleteItemFromData('sync-posts', resData.id);
+                });
+              }
+            })
+            .catch((err) => {
+              console.log('Error while sending data', err);
+            });
+        }
+      })
+    );
+  }
 });
 
 self.addEventListener('notificationclick', (e) => {
-	var notification = e.notification;
-	var action = e.action;
-	console.log(notification);
-	if (action === 'confirm') {
-		console.log('Confirm was chosen');
-		notification.close();
-	} else {
-		console.log(action);
-	}
+  var notification = e.notification;
+  var action = e.action;
+  console.log(notification);
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+    notification.close();
+  } else {
+    console.log(action);
+  }
 });
 
 self.addEventListener('notificationclose', (e) => {
-	console.log('Notification was closed', e);
+  console.log('Notification was closed', e);
 });
 //self.addEventListener('sync', (e) => {
 //	console.log('[SERVICE WORKER] Background syncing', e);
